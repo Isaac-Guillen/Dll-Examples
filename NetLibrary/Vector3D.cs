f916febbe9eced602f8f8c906188765a7feb996f
Isaac <isaac.guillencast@gmail.com>
2020-09-07 10:57:18 -0500

    Files restored but ignoring unnecesary directories

---
+++ NetLibrary/Vector3D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NetLibrary
+{
+    public class Vector3D
+    {
+        public double x;
+        public double y;
+        public double z;
+
+        Vector3D(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double Lenght 
+        {
+            get
+            {
+                return this.CalculateLenght();
+            }
+        }
+
+        private double CalculateLenght()
+        {
+            return Math.Sqrt(this.x*this.x + this.y*this.y + this.z+this.z);
+        }
+    }
+}
